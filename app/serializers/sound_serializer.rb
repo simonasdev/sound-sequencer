ac8b7a0438707f58a2e697c96930713a4aad10a6
Simonas Gildutis <simonasgildutis@gmail.com>
2018-10-13 18:58:40 +0300

    ghgh

--- app/serializers/sound_serializer.rb
+++ app/serializers/sound_serializer.rb
@@ -1,10 +1,9 @@
 class SoundSerializer
   include FastJsonapi::ObjectSerializer
-  include Rails.application.routes.url_helpers
 
   attributes :id, :data
 
   attribute :image_url do |object|
-    url_for(object.file) if object.file
+    Rails.application.routes.url_helpers.url_for(object.file) if object.file
   end
 end
