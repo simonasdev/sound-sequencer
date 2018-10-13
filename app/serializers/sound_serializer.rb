4518be7b5a2794f11cd4229518d62ed3c0705f7d
Povilas <mobilaizeris@gmail.com>
2018-10-13 21:29:07 +0300

    name fix

--- app/serializers/sound_serializer.rb
+++ app/serializers/sound_serializer.rb
@@ -3,11 +3,11 @@ class SoundSerializer
 
   attributes :data
 
-  attribute :image_url do |object|
+  attribute :file_url do |object|
     Rails.application.routes.url_helpers.url_for(object.file) if object.file.attached?
   end
 
   attribute :filename do |object|
-    object.file.name if object.file.attached?
+    object.file.blob.filename if object.file.attached?
   end
 end
