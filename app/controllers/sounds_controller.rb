13e3a2dfe79c2819720dd5867decc1a66d7436eb
Povilas <mobilaizeris@gmail.com>
2018-10-13 16:57:43 +0300

    action create

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -1,5 +1,19 @@
 class SoundsController < ApplicationController
   def index
+    sounds = Sound.all
+  end
 
+  def create
+    Sound.upload_file(:params)
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def sound_params
+    params.require(:sound).permit!
   end
 end
