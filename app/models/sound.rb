15f4f43c19f673d7762863e823e71d9c80f23372
Simonas Gildutis <simonasgildutis@gmail.com>
2018-10-13 20:13:01 +0300

    logger

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -5,7 +5,9 @@ class Sound < ApplicationRecord
 
   def upload_file params
     decoded = Base64.decode64(params[:data].to_s)
-
+    Rails.logger.info params.slice(:filename, :content_type).merge(
+        io: DataStringIO.new(params[:filename], params[:content_type], decoded)
+      )
     file.attach(
       params.slice(:filename, :content_type).merge(
         io: DataStringIO.new(params[:filename], params[:content_type], decoded)
