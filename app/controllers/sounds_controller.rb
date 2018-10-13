042c06e43f7eb247d82e674dc2d078ce52f500bb
Povilas <mobilaizeris@gmail.com>
2018-10-13 17:50:02 +0300

    something in controller

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -6,15 +6,28 @@ class SoundsController < ApplicationController
 
   def create
     Sound.upload_file(:params)
+    render json: '{message: "Created! Thanks!"}'
   end
 
   def edit
+    sound = Sound.find(params[:id])
+    render json: SoundSerializer.new(sound)
+  end
 
+  def update
+    sound = Sound.upload_file(params)
+    # sound.update(params)  Not sure how do we update and what params we receive
+  end
+
+  def destroy
+    sound = Sound.find(params[:id])
+    sound.destroy
+    render json: '{message: "Deleted!"}'
   end
 
   private
 
   def sound_params
-    params.require(:sound).permit!
+    params.require(:data).permit!
   end
 end
--- app/models/sound.rb
+++ app/models/sound.rb
@@ -11,5 +11,4 @@ class Sound < ApplicationRecord
       )
     )
   end
-
 end
