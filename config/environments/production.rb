c6fe5317a82944c18afd72206b3d8a0d1ad1e357
Simonas Gildutis <simonasgildutis@gmail.com>
2018-10-13 18:35:35 +0300

    url host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,10 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = ENV['HOST'] || 'localhost:3000'
+  config.action_controller.default_url_options = {
+    host: config.action_controller.asset_host
+  }
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
