dbb9676671c4e23f547b75415ef9c632c6ef1825
Simonas Gildutis <simonasgildutis@gmail.com>
2018-10-13 19:00:55 +0300

    host?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = ENV['HOST'] || 'localhost:3000'
+  config.action_mailer.default_url_options = {
   config.action_controller.default_url_options = {
     host: config.action_controller.asset_host
   }
